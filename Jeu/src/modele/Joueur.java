037efb7b1b64b217cf406ec209909ffdeaaa3d06
Nodal2 <bruneauoscar@gmail.com>
2019-05-09 16:13:16 +0200

    Personnage et début de controleur

---
+++ Jeu/src/modele/Joueur.java
@@ -0,0 +1,12 @@
+package modele;
+
+public class Joueur extends Personnage{
+
+	public Joueur(int vie, double vitesse, int taille, String nom, int x, int y) {
+		super(vie, vitesse, taille, nom, x, y);
+	}
+
+	
+	
+	
+}
