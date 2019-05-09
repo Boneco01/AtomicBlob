9b00adcb03c3db60a64be28e3440c580f392b1bd
Boneco01 <rynkiewicz.paul@gmail.com>
2019-05-09 15:57:09 +0200

    Commit pour Block

--- Jeu/src/modele/Block.java
+++ Jeu/src/modele/Block.java
@@ -1,6 +1,15 @@
 package modele;
 
 public class Block {
+	
 	private char id;
+	private int taille;
+	private String sprite;
+	
+	public Block(char id) {
+		this.id = id;
+		this.taille = 16;
+		this.sprite = spriteById();
+	}
 	
 }
