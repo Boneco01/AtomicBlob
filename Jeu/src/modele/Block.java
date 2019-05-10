b9306902a0e656dd41b5e2aa0e4d0f95905934fc
qchastel <qchastel@iut.univ-paris8.fr>
2019-05-10 16:05:26 +0200

    Ajout Terrain depuis fichier

--- Jeu/src/modele/Block.java
+++ Jeu/src/modele/Block.java
@@ -3,13 +3,12 @@ package modele;
 public class Block {
 	
 	private char id;
-	private int taille;
-	private String sprite;
 	
 	public Block(char id) {
 		this.id = id;
-		this.taille = 16;
-		this.sprite = spriteById();
+	}
+	public String toString() {
+		return id+"";
 	}
 	
 }
--- Jeu/src/modele/Terrain.java
+++ Jeu/src/modele/Terrain.java
@@ -1,9 +1,39 @@
 package modele;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 public class Terrain {
 
-	private ObservableList<Block> map;
-	
+	private ObservableList<Block> map = FXCollections.observableList(new ArrayList());
+	public int tailleRecupMap(String adresseFichierMap) {
+		int tailleLigne = -1;
+		String line;
+		try {
+		File fichierMap = new File(adresseFichierMap);
+		FileReader fr = new FileReader(fichierMap);
+        BufferedReader br = new BufferedReader(fr);
+        for (line = br.readLine(); line != null; line = br.readLine()) {
+            String tableauChaine[] =  line.split(":");
+            for(int i=0;i<tableauChaine.length;i++) 
+            	map.add(new Block(tableauChaine[i].charAt(0)));
+            	tailleLigne=line.split(":").length;
+            
+        }
+        fr.close();
+        br.close();
+        
+		}
+		catch(Exception E) {
+			E.printStackTrace();
+		}
+		return tailleLigne;
+		
+	}
+	public ObservableList<Block> getMap() {
+		return map;
+	}
 }
